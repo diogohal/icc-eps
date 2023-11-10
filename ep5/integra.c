ee53d7e88197ce9bd746acd2d5163225d22c3018
Diogo Almeida <dha21@inf.ufpr.br>
2023-11-10 16:57:40 -0300

    aaa

--- ep5/integra.c
+++ ep5/integra.c
@@ -17,9 +17,9 @@ double monteCarlo(double a, double b, int n_dimensoes)
 {
   double resultado;
   double soma = 0.0;  
+  double h = b - a;
   
   // Monte Carlo algoritmo para a função Styblinski-Tang
-  
   double x , sum = 0;
   for(int j = 0; j < n_dimensoes; j++) {
     for ( int i =0; i < PONTOS ; i ++) {
@@ -27,7 +27,7 @@ double monteCarlo(double a, double b, int n_dimensoes)
       sum += (x*x*x*x - 16*x*x + 5*x)/2;
     }
   }
-  resultado = ( sum / PONTOS ) * ( b - a );
+  resultado = ( sum / PONTOS ) * pow(h, n_dimensoes);
 
   if(resultado < 0)
     resultado = resultado * -1;
@@ -38,22 +38,29 @@ double monteCarlo(double a, double b, int n_dimensoes)
 double retangulos_xy(double a, double b, int n_dimensoes) {
 
   double h;
-  double x = 0.0;
+  double x, y = 0.0;
   double soma = 0.0;
-  
+  double xx = 0.0;
+  double yy = 0;
   // MÉTODO DO RETÂNGULO
   h = (b - a)/PONTOS;
   printf("%f\n", h);
-  for(int j = 0; j < n_dimensoes; j++) {
-    for(int i = 0; i < PONTOS; ++i) {
-      x = a + h*i;
-      soma += (x*x*x*x - 16*x*x + 5*x)/2*h;
+  x = a;
+  for(long long int j = 0; j < PONTOS; ++j) {
+    y = a;
+    for(long long int i = 0; i < PONTOS; ++i) {
+      xx = x*x;
+      yy = y*y;
+      soma += (xx*xx - 16*xx + 5*x)/2;
+      soma += (yy*yy - 16*yy + 5*y)/2;
+      y += h;
     }
+    x += h;
   }
   
   if(soma < 0)
     soma = soma*-1;
-  return soma;
+  return soma * h*h;
 }
 
 
