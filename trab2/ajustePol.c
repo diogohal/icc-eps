4320b9d72aee32403ab0637158ec1e7721d1a4d9
diogohal <dha21@inf.ufpr.br>
2023-11-29 18:41:41 -0300

    aaa

--- trab2/ajustePol.c
+++ trab2/ajustePol.c
@@ -18,12 +18,13 @@ int main() {
     LIKWID_MARKER_INIT;
     double start, stop, tgeraSL, tsolSL, tResiduo = 0.0;
     long long int n, k;
-    fscanf(stdin, "%lld %lld", &n, &k);
+    int returnScanf = 0;
+    returnScanf = fscanf(stdin, "%lld %lld", &n, &k);
 
     pontos_t *pontos = malloc(sizeof(pontos_t) * k);
 
     for (long long int i = 0; i < k; i++) {
-        fscanf(stdin, "%lf %lf", &pontos[i].x, &pontos[i].y);
+        returnScanf = fscanf(stdin, "%lf %lf", &pontos[i].x, &pontos[i].y);
     }
 
     matriz_t *SL = criaMatriz(n + 1);
@@ -45,10 +46,11 @@ int main() {
     //     printaIntervalo(&SL->X[i]);
     //     printf(" ");
     // }
-    printf("\n");
 
     start = timestamp();
+    LIKWID_MARKER_START("calcRes");
     intervalo_t *residuo = calculaResiduoNaive(SL, pontos, k);
+    LIKWID_MARKER_STOP("calcRes");
     stop = timestamp();
     tResiduo = stop - start;
     // for (int i = 0; i < k; i++) {
@@ -57,7 +59,9 @@ int main() {
     // }
     // printf("\n%f\n", tResiduo);
 
-    printf("\n%f\n", tgeraSL);
+    printf("geraSL = %f\n", tgeraSL);
+    printf("solSL = %f\n", tsolSL);
+    printf("calcRes = %f\n", tResiduo);
     // printf("%f\n", tsolSL);
     LIKWID_MARKER_CLOSE;
 
